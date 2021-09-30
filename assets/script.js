41d15791575387b0bf54dcfab0b66052e803c3cf
Adrian Reyes <adrian_reyes1@yahoo.com>
2021-09-29 21:34:46 -0700

    local storage currently broken due to total. cell no longer goes into negative

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ yourName = 'Adrian Reyes';
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-let total = 0
+let total = 0   // Total
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
@@ -29,50 +29,100 @@ document.getElementById('add-gb').addEventListener('click', function(){
 
 let totalCell =  document.querySelector('#qty-total');
 
+let allTotal = localStorage.getItem('all-total')
+if(allTotal){
+    totalCell.textContent = allTotal
+}
+
 //Ginger bread
 
-gbCell =  document.querySelector('#qty-gb');
+let gbCell =  document.querySelector('#qty-gb');
+
+let gbAdd = localStorage.getItem('gb-total')
+if(gbAdd){
+    gbCell.textContent = gbAdd
+}
 
 document.getElementById('add-gb').addEventListener('click', function(){
     gb += 1;
     gbCell.textContent = gb;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('gb-total', gb)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
 
+let gbMinus = localStorage.getItem('gb-total')
+if(gbMinus){
+    gbCell.textContent = gbMinus
+}
+
 document.getElementById('minus-gb').addEventListener('click', function(){
-    gb -= 1;
+    gb = Math.max(1, gb)
+    gb = localStorage.getItem('gb-total') - 1;
     gbCell.textContent = gb;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('gb-total', gb)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
 
 // chocolate chip
 
-ccCell =  document.querySelector('#qty-cc');
+let ccCell =  document.querySelector('#qty-cc');
+
+let ccAdd = localStorage.getItem('cc-total')
+if(ccAdd){
+    ccCell.textContent = ccAdd
+}
 
 document.getElementById('add-cc').addEventListener('click', function(){
     cc += 1;
     ccCell.textContent = cc;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('cc-total', cc)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
 
+let ccMinus = localStorage.getItem('cc-total')
+if(ccMinus){
+    ccCell.textContent = ccMinus
+}
+
 document.getElementById('minus-cc').addEventListener('click', function(){
-    cc -= 1;
+    cc = Math.max(1, cc)
+    cc = localStorage.getItem('cc-total') - 1;
     ccCell.textContent = cc;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('cc-total', cc)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
 
 // sugar sprinkle
 
-sugarCell =  document.querySelector('#qty-sugar');
+let sugarCell =  document.querySelector('#qty-sugar');
+
+let sugarAdd = localStorage.getItem('sugar-total')
+if(sugarAdd){
+    sugarCell.textContent = sugarAdd
+}
 
 document.getElementById('add-sugar').addEventListener('click', function(){
     sugar += 1;
     sugarCell.textContent = sugar;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('sugar-total', sugar)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
 
+let sugarMinus = localStorage.getItem('sugar-total')
+if(sugarMinus){
+    sugarCell.textContent = sugarMinus
+}
+
 document.getElementById('minus-sugar').addEventListener('click', function(){
-    sugar -= 1;
+    sugar = Math.max(1, sugar)
+    sugar = localStorage.getItem('sugar-total') - 1;
     sugarCell.textContent = sugar;
     totalCell.textContent = gb + cc + sugar;
+    localStorage.setItem('sugar-total', sugar)
+    localStorage.setItem('all-total', totalCell.textContent)
 })
